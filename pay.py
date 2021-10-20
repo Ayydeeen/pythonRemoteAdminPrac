11b0b111ecb6a0d60b6368600322c14c3f4f7d60
Aidan Brubeck <50123545+Ayydeeen@users.noreply.github.com>
2021-10-19 22:13:40 -0400

    Update pay.py
    Updated using realpython.com with statement socket functionality

--- pay.py
+++ pay.py
@@ -1,36 +1,18 @@
+#!/usr/bin/env python3
 import socket
-import time
 
-#Variable Declaration
-server = "127.0.0.1"
-port = 4445
+HOST = "192.168.19.129"
+PORT = 65432
 
-#Functions
-def send(msg):
-        s.send(msg.encode("UTF-8"))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b'test')
+        data = s.recv(1024)
 
-def getInstruction():
-        while True:
-                msg = s.recv(4096)
-                cmd = msg.decode("UTF-8")
+print('Received', repr(data))
 
-                if (cmd) == "test":
-                        try:
-                                send("[OK]")
-                        except:
-                                print("error on send")
-                                pass
 
-#Connection
-s = socket.socket((socket.AF_INET), socket.SOCK_STREAM)
-host = socket.gethostname()
-connected = False
 
-while connected == False:
-        try:
-                s.connect((host, port))
-                connection = True
-        except:
-                time.sleep(30)
-print("connected")
-getInstructions()
+
+
+
