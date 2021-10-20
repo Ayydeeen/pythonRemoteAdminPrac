61e92eab2fc9d32a8fb34ed3d45a71515f38c93e
Aidan Brubeck <50123545+Ayydeeen@users.noreply.github.com>
2021-10-19 21:46:22 -0400

    Update serv.py
    realpython.com blowing my mind with with statement context management. Re-learning how to program sockets

--- serv.py
+++ serv.py
@@ -1,40 +1,19 @@
-  GNU nano 5.1                                                                                                                                                           serv.py                                                                                                                                                                     
+  GNU nano 5.1                                                                                                                                                           serv.py                                                                                                                                                           Modified  
+#!/usr/bin/env python3
 import socket
-import os
 
-#Variable Declaration
-port=4445
+HOST = "127.0.0.1"
+PORT = "65432"
 
-def clear():
-        os.system('cls' if os.name=='nt' else 'clear')
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+                print('Connected by', addr)
+                while True:
+                        data = conn.recv(1024)
+                        if not data:
+                                break
+                        conn.sendall(data)
 
-
-#Begin Listening for Connection
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
-serversocket.bind((host,port))
-serversocket.listen(1)
-
-clear()
-
-print("Basic Remote Administration Tool")
-clientsocket, addr = serversocket.accept()
-print("Connection from: " + str(addr))
-
-while True:
-        cmd = input("$: ")
-
-        if cmd == "help":
-                print("Available Commands:    ")
-                print("    test : Test server connection to client")
-
-        elif cmd == "exit":
-                clientsocket.shutdown(socket.SHUT_RDWR)
-                clientsocket.close()
-
-        else:
-                ecmd = cmd.encode("UTF-8")
-                clientsocket.send(ecmd)
-
-                ercv = clientsocket.recv(4096)
-                print(ercv.decode("UTF-8"))
