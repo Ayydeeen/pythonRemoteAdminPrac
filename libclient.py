19317d968f142bb968887b0cd9021bad6f4e1ed1
Aidan Brubeck <50123545+Ayydeeen@users.noreply.github.com>
2021-11-25 13:41:51 -0500

    Update libclient.py

--- libclient.py
+++ libclient.py
@@ -96,6 +96,9 @@ class Message:
         return json.dumps(obj, ensure_ascii=False).encode(encoding)
 
     def _json_decode(self, json_bytes, encoding):
+        print(encoding)
+        print(json_bytes)
+        print(encoding)
         tiow = io.TextIOWrapper(io.BytesIO(json_bytes), encoding=encoding, newline="")
         obj = json.load(tiow)
         tiow.close()
